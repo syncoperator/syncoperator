0bc56e99a58caaddd50d0b839955797f1e1f89b0
syncoperator <russianflow@icloud.com>
2026-02-03 05:59:33 +0100

    Update storage.js

--- liste/storage.js
+++ liste/storage.js
@@ -1,13 +1,6 @@
-const DB_KEY = 'QS_CENTRAL_DB_V2';
+const DB_KEY = 'QS_CENTRAL_FINAL_V1';
 let db = JSON.parse(localStorage.getItem(DB_KEY)) || [];
 
 function saveDB() {
     localStorage.setItem(DB_KEY, JSON.stringify(db));
-}
-
-function cleanProjectSlots(pIdx) {
-    if (db[pIdx] && db[pIdx].tools) {
-        db[pIdx].tools = db[pIdx].tools.filter(t => t.id && t.id.trim() !== "");
-    }
-    saveDB();
 }
