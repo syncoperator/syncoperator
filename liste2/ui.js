28a0acf0ee50e7f547791688fa826b345f7f5d3c
syncoperator <russianflow@icloud.com>
2026-02-05 20:25:39 +0100

    ui.js aktualisieren

--- liste2/ui.js
+++ liste2/ui.js
@@ -245,7 +245,7 @@ function makePDF() {
             </div>
 
             <div style="border-top:1px solid #000; padding-top:5px; font-size:9px; font-weight:800; text-align:center; color:#666;">
-                QS CENTRAL PREMIUM REPORT
+                
             </div>
         </div>
     </div>`;
